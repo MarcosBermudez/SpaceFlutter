c6b7ee094263780dcb5b9b0d544a244180bc8e49
Marcos Bermudez <marcos@bermudez.fr>
2017-09-15 15:12:09 +0200

    Add ship image

--- lib/SapceObjects.dart
+++ lib/SapceObjects.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/services.dart';
 
+// Images from
+// https://opengameart.org/content/space-ship-construction-kit
+// https://opengameart.org/content/25-special-effects-rendered-with-blender
 class HasTurn {
-  Positioned performTurn(int deltaTimeSinceLastTurn, double tapX, double tapY) => null;
+  Positioned performTurn(
+          int deltaTimeSinceLastTurn, double tapX, double tapY) =>
+      null;
 
   bool isMissile() => false;
 
@@ -15,7 +22,7 @@ class HasTurn {
   double height = 15.0;
 }
 
-class Spaceship extends StatelessWidget implements HasTurn {
+class Spaceship extends StatefulWidget implements HasTurn {
   Spaceship([this.color = Colors.blue]);
 
   final Color color;
@@ -28,29 +35,30 @@ class Spaceship extends StatelessWidget implements HasTurn {
   double height = 50.0;
   Positioned lastPosition;
 
-  //Painter
-  SpaceshipPainter painter = new SpaceshipPainter(50.toDouble());
+  _ShipSpaceshipState state = new _ShipSpaceshipState();
 
   @override
   Positioned performTurn(int deltaTimeSinceLastTurn, double tapX, double tapY) {
-    if (lastPosition != null && tapX != null) {
+    if (lastPosition != null && tapX != null && !state.isImpacted()) {
       double oldTop = lastPosition.top;
       double oldLeft = lastPosition.left;
 
-      int distanceMoved = ((spaceshipSpeed * deltaTimeSinceLastTurn) /1000).round();
+      int distanceMoved =
+          ((spaceshipSpeed * deltaTimeSinceLastTurn) / 1000).round();
 
       // Calculate new position
       lastTop =
           tapY > oldTop ? lastTop + distanceMoved : lastTop - distanceMoved;
-      lastLeft = tapX > oldLeft
-          ? lastLeft + distanceMoved
-          : lastLeft - distanceMoved;
+      lastLeft =
+          tapX > oldLeft ? lastLeft + distanceMoved : lastLeft - distanceMoved;
 
       // If distance is less than we can move, move directly to target
-      if((oldTop-tapY).abs()<distanceMoved) lastTop=tapY;
-      if((oldLeft-tapX).abs()<distanceMoved) lastLeft=tapX;
+      if ((oldTop - tapY).abs() < distanceMoved) lastTop = tapY;
+      if ((oldLeft - tapX).abs() < distanceMoved) lastLeft = tapX;
     }
 
+    // Inform of new turn in state;
+    state.performTurn(deltaTimeSinceLastTurn);
 
     Positioned newPosition =
         new Positioned(top: lastTop, left: lastLeft, child: this);
@@ -62,57 +70,65 @@ class Spaceship extends StatelessWidget implements HasTurn {
   bool isMissile() => false;
 
   @override
-  bool isGoneOfSpace(double width, double height) => false;
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    painter.color = color;
-    return new CustomPaint(
-      size: new Size(width, height),
-      painter: painter,
-    );
-  }
+  bool isGoneOfSpace(double width, double height) => state.isDestroyed();
 
   @override
   void impacted(HasTurn object) {
-    painter.color = Colors.black;
+      state.impacted();
+  }
+
+  @override
+  State<Spaceship> createState() {
+    return state;
   }
 }
 
-class SpaceshipPainter extends CustomPainter {
-  static const barWidth = 50.0;
+class _ShipSpaceshipState extends State<Spaceship> {
+  int impactedTurn = 0;
 
-  SpaceshipPainter(this.barHeight);
+  Image image = new Image.asset('assets/images/ships/1.png');
 
-  final double barHeight;
-  Color color = Colors.blue;
+  int deltaSinceLastState = 0;
+  int timePerState = 50;
 
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = new Paint()
-      ..color = this.color
-      ..style = PaintingStyle.fill;
-    canvas.drawRect(
-      new Rect.fromLTWH(
-        (size.width - barWidth) / 2.0,
-        size.height - barHeight,
-        barWidth,
-        barHeight,
-      ),
-      paint,
-    );
+  void impacted() {
+    if (!isImpacted()) {
+      setState(() {
+        impactedTurn = impactedTurn + 1;
+      });
+    }
+  }
+
+  performTurn(int deltaTimeSinceLastTurn) {
+    deltaSinceLastState = deltaSinceLastState + deltaTimeSinceLastTurn;
+    if (isImpacted() && deltaSinceLastState > timePerState) {
+      setState(() {
+        impactedTurn = impactedTurn + 1;
+        deltaSinceLastState = 0;
+      });
+    }
   }
 
   @override
-  bool shouldRepaint(SpaceshipPainter old) => color != old.color;
+  Widget build(BuildContext context) {
+    if (isImpacted() && !isDestroyed()) {
+      image = new Image.asset(
+          'assets/images/ships/' + impactedTurn.toString() + '.png');
+      impactedTurn++;
+    }
+    return image;
+  }
+
+  bool isImpacted() => impactedTurn != 0;
+
+  bool isDestroyed() => impactedTurn == 10;
 }
 
 class Missile extends StatelessWidget implements HasTurn {
   Missile([this.color = Colors.red]);
 
   final Color color;
-  final int speed = 150;// Speed in pixels by second
+  final int speed = 150; // Speed in pixels by second
 
   double lastTop = 0.0;
   double lastLeft = 100.0;
@@ -129,8 +145,7 @@ class Missile extends StatelessWidget implements HasTurn {
 
   @override
   Positioned performTurn(int deltaTimeSinceLastTurn, double tapX, double tapY) {
-
-    int distanceMoved = ((speed * deltaTimeSinceLastTurn) /1000).round();
+    int distanceMoved = ((speed * deltaTimeSinceLastTurn) / 1000).round();
 
     lastTop = lastTop + distanceMoved;
 
@@ -162,6 +177,7 @@ class MissilePainter extends CustomPainter {
     final paint = new Paint()
       ..color = this.color
       ..style = PaintingStyle.fill;
+
     canvas.drawRect(
       new Rect.fromLTWH(
         (size.width - barWidth) / 2.0,
